5868d7bef0a23f1664ab1891d70fed81207b25cb
TimurTsokov <tsokov2t1m5@gmail.com>
2018-09-13 19:01:01 +0300

    foliac fin

---
+++ js/jscript.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $(window).on('scroll', function () {
+        let scrollTop = $('html, body').scrollTop();
+        if (scrollTop > 500) {
+            $('.logo').fadeOut();
+        } else {
+            $('.logo').fadeIn();
+        }
+    });
+
+    $('.menu-toggler').on('click', function (e) {
+        e.preventDefault();
+        $('body').toggleClass('menu-open');
+    });
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]')
+        .on('click', function (e) {
+            e.preventDefault();
+            $('body').removeClass('menu-open');
+        })
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > '600') {
+            $('header').css({"background-color": "rgba(25, 189, 154, 0.7)"});
+            $('nav').css({"padding": "0"})
+        } else if ($(window).scrollTop() < '600') {
+            $('header').css({"background-color": "transparent"});
+            $('nav').css({"padding": "2rem 0"})
+        }
+    })
+});
